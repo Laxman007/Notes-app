9b1726cecbf0b253c5162651a8ee85b652e0b0d6
Laxman007 <lakshmangoyalladwa@gmail.com>
2020-06-24 16:41:10 +0530

    Adding  add note feature

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const chalk=require('chalk')
 const yargs=require('yargs')
-
+const notes=require('./notes.js')
 
 // create add command
 yargs.command({
@@ -21,8 +21,7 @@ yargs.command({
 
     },
     handler:function(argv){
-        console.log('title : '+argv.title)
-        console.log('body : '+argv.body)
+       notes.addNote(argv.title,argv.body)
     }
 })
 
